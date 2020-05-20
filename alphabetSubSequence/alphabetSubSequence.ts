8859ad818836c6e1697f2ede6abbd08ebba0ed23
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-05-20 11:39:10 +0200

    equally strong

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,14 @@
-function alphabetSubsequence(s: string): boolean {
+function alphabetSubsequence(arr: string): boolean {
+	let subsequenceSet = new Set();
+	for (let letter of arr) {
+		subsequenceSet.add(letter);
+	}
 
+	return subsequenceSet.size === arr.length;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
